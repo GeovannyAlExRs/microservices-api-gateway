34cd845fe227d078705e915bf2ea2e67a25f3168
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-17 15:56:02 -0500

    Create UsersRepository, add method findByUsername and updateUserRole

---
+++ src/main/java/com/company/dtk/microservicesapigateway/model/repository/UsersRepository.java
@@ -0,0 +1,19 @@
+package com.company.dtk.microservicesapigateway.model.repository;
+
+import com.company.dtk.microservicesapigateway.model.Roles;
+import com.company.dtk.microservicesapigateway.model.Users;
+import feign.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByUsername(String username);
+
+    @Modifying
+    @Query("update Users set role=:role where usename=:usename")
+    void updateUserRole(@Param("username") String username, @Param("role") Roles role);
+}
