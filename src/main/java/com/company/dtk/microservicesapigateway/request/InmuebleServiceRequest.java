f6c10d86b78036f4b17aefcb9d3758988d4e50db
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-19 12:40:53 -0500

    create interface InmuebleServiceRequest

---
+++ src/main/java/com/company/dtk/microservicesapigateway/request/InmuebleServiceRequest.java
@@ -0,0 +1,24 @@
+package com.company.dtk.microservicesapigateway.request;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(
+        value = "inmueble*service",
+        path = "/api/inmueble",
+        url = "${inmueble.service.url}",
+        configuration = FeignConfiguration.class
+)
+public interface InmuebleServiceRequest {
+
+    @PostMapping
+    Object createInmueble(@RequestBody Object requestObject);
+
+    @DeleteMapping
+    void deleteInmueble(@PathVariable("inmuebleId") Long inmuebleId);
+
+    @GetMapping()
+    List<Object> getAllInmueble();
+}
