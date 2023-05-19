079021121e3766e66cde9837247040bb0482ff16
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-19 15:52:14 -0500

    Create CompraServiceRequest with methods

---
+++ src/main/java/com/company/dtk/microservicesapigateway/request/CompraServiceRequest.java
@@ -0,0 +1,24 @@
+package com.company.dtk.microservicesapigateway.request;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(
+        value = "compra-service",
+        path = "/api/compra",
+        url = "${compra.service.url}",
+        configuration = FeignConfiguration.class
+)
+public interface CompraServiceRequest {
+
+    @PostMapping
+    Object createCompra(@RequestBody Object compra);
+
+    @GetMapping("{userId}")
+    List<Object> getAllCompraOfUser(@PathVariable("userId") Long userId);
+}
